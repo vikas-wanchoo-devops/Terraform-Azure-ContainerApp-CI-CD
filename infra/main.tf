60ac2dfcf9863abcb5f9d4108be1ed960573ebd3
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-03-07 16:37:46 +0530

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,38 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "WanchooResourceGroup1"
+  location = "East US"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "vikasacr1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_container_app_environment" "env" {
+  name                = "vikas-env1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_container_app" "flaskapi" {
+  name                         = "flaskapi-app1"
+  resource_group_name          = azurerm_resource_group.rg.name
+  container_app_environment_id = azurerm_container_app_environment.env.id
+
+  template {
+    container {
+      name   = "flaskapi"
+      image  = "${azurerm_container_registry.acr.login_server}/flask-api:latest"
+      cpu    = 0.5
+      memory = "1Gi"
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 5000
+  }
+}
