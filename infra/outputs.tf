cac54c7fc163d72183a3e31cc388aca1f59d8fbb
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-03-07 18:45:57 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,7 @@
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
 output "container_app_url" {
   value = azurerm_container_app.flaskapi.latest_revision_fqdn
 }
