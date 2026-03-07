c02745f09dac2c4c3dd0a35ffd6e3a7f8e08a030
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-03-07 16:38:08 +0530

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "WanchooResourceGroup1"
+}
+
+variable "acr_name" {
+  default = "vikasacr1"
+}
+
+variable "env_name" {
+  default = "vikas-env1"
+}
+
+variable "app_name" {
+  default = "flaskapi-app1"
+}
