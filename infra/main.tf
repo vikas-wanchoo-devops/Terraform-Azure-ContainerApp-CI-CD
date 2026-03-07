eba3f7be059613a9ca642a1cc230c66f7a29ac49
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-03-07 19:01:37 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,19 +1,23 @@
+locals {
+  location = var.preferred_locations[0]
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-  location = var.location
+  location = local.location
 }
 
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
+  location            = local.location
   sku                 = "Basic"
   admin_enabled       = true
 }
 
 resource "azurerm_container_app_environment" "env" {
   name                = var.env_name
-  location            = var.location
+  location            = local.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
